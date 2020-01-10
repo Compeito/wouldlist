43475ce9d761d5cf9cf6b1cc154e914680dda6ca
Compeito <com0806peito@icloud.com>
2020-01-11 01:22:25 +0900

    axiosの型設定追加

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,31 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <b-button @click="increment">{{ count }}</b-button>
-    </div>
-  </section>
+  <div class="content">
+    <p>{{ message }}</p>
+    <b-button @click="increment">{{ count }}</b-button>
+  </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator'
+import {Component, Vue} from 'nuxt-property-decorator'
+
 
 @Component
 export default class index extends Vue {
   count: number = 0;
+  message: string = "loading...";
+
+  mounted() {
+    interface YesNoResponse {
+      answer: string,
+      forced: boolean,
+      image: string,
+    }
+
+    this.$axios.get('https://yesno.wtf/api')
+      .then((result: { data: YesNoResponse }) => {
+        this.message = result.data.answer;
+      })
+  }
 
   increment(): void {
     this.count++;
