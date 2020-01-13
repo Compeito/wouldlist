89689cfadb5b086dd333ec00de5059e9227454ce
Compeito <com0806peito@icloud.com>
2020-01-13 22:10:59 +0900

    __pycache__追加

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 import logging
 
 import firebase_admin
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, Depends
 from firebase_admin import auth as firebase_auth
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
@@ -27,7 +27,14 @@ register_tortoise(app, settings.DB_CONFIG, generate_schemas=True)
 app.include_router(users.router, prefix='/users', tags=['users api'])
 
 
-@app.get('/me')
-async def login(request: Request, authorization: str = Header(None)):
+def get_user(authorization: str = Header(None)):
     token = authorization.split(' ')[-1]
-    return firebase_auth.verify_id_token(token)
+    try:
+        return firebase_auth.verify_id_token(token)
+    except Exception as e:
+        return {'error': str(type(e)) + ': ' + str(e)}
+
+
+@app.get('/me')
+async def login(request: Request, user: dict = Depends(get_user)):
+    return user
