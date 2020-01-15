9fcebf4a5065f93069d1ccb6bfbd4e8d2f1f0e40
Compeito <com0806peito@icloud.com>
2020-01-15 09:45:25 +0900

    更新

--- backend/app/users/depends.py
+++ backend/app/users/depends.py
@@ -7,16 +7,18 @@ from app.users.schemes import FirebaseToken
 
 
 def get_token(authorization: str = Header(None)) -> FirebaseToken:
+    if authorization is None:
+        raise HTTPException(status_code=401, detail='認証情報がありません')
     token = authorization.split(' ')[-1]
     try:
         token_dict = firebase_auth.verify_id_token(token)
         return FirebaseToken(**token_dict)
     except firebase_auth.InvalidIdTokenError as e:
-        raise HTTPException(status_code=403, detail='トークンが不正です')
+        raise HTTPException(status_code=401, detail='トークンが不正です')
 
 
 async def get_user(token: FirebaseToken = Depends(get_token)) -> User:
     try:
         return await User.get(uid=token.uid)
     except tortoise.exceptions.DoesNotExist:
-        raise HTTPException(status_code=403, detail='ユーザーが存在しません')
+        raise HTTPException(status_code=401, detail='ユーザーが存在しません')
