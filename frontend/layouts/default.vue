eed74ef539702cebda6f6d94dea4b6ebef2050f3
Compeito <com0806peito@icloud.com>
2020-01-19 00:50:22 +0900

    更新

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,24 +2,15 @@
   <div>
     <sui-menu fixed="top" inverted>
       <sui-container>
-        <sui-menu-item header>
-          <nuxt-link to="/">
-            WouldList
-          </nuxt-link>
-        </sui-menu-item>
-        <sui-menu-item link href="#">
-          Home
-        </sui-menu-item>
-        <sui-dropdown item text="Account Menu">
-          <sui-dropdown-menu>
-            <nuxt-link to="/login" class="item">
-              login
-            </nuxt-link>
-            <nuxt-link to="/logout" class="item">
-              logout
-            </nuxt-link>
-          </sui-dropdown-menu>
-        </sui-dropdown>
+        <nuxt-link to="/" class="item header">
+          WouldList
+        </nuxt-link>
+        <nuxt-link to="/login" class="item">
+          login
+        </nuxt-link>
+        <nuxt-link to="/logout" class="item">
+          logout
+        </nuxt-link>
       </sui-container>
     </sui-menu>
     <div class="main">
