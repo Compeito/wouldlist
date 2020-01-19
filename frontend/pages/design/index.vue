c00f65abfca8b8d9114b2b4be488ae0f81d18c0d
Compeito <com0806peito@icloud.com>
2020-01-20 00:58:51 +0900

    更新

--- frontend/pages/design/index.vue
+++ frontend/pages/design/index.vue
@@ -1,12 +1,20 @@
 <template>
-  <sui-container>
+  <sui-container text>
+    <h1 class="ui center aligned icon header">
+      <sui-image src="https://github.com/Compeito.png" circular />
+      <sui-header-content>Compeitoのやってほしいことリスト</sui-header-content>
+    </h1>
+    <sui-divider hidden class="section" />
     <sui-card-group :items-per-row="3" stackable>
       <template v-for="(item, i) in items">
-        <nuxt-link :key="i" :to="`/todo?id=${item.title}`" class="ui card raised link">
+        <nuxt-link :key="i" to="/design/1" class="ui card raised link">
           <sui-card-content>
             <sui-card-header>{{ item.title }}</sui-card-header>
             <sui-card-description>{{ item.text }}</sui-card-description>
           </sui-card-content>
+          <sui-button attached="bottom">
+            <sui-icon name="add" /> ボタン
+          </sui-button>
         </nuxt-link>
       </template>
     </sui-card-group>
