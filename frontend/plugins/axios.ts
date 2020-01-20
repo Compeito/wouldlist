c37ef5917f349fdd6f889146c6e9c6a4f5523057
Compeito <com0806peito@icloud.com>
2020-01-21 01:03:48 +0900

    更新

--- frontend/plugins/axios.ts
+++ frontend/plugins/axios.ts
@@ -1,12 +1,17 @@
-import { Plugin } from '@nuxt/types'
+import { Context, Plugin } from '@nuxt/types'
+import { AxiosRequestConfig } from 'axios'
+
 import auth from '~/plugins/auth'
 
-const axiosPlugin: Plugin = async ({ $axios }) => {
-  const user = await auth()
-  if (user) {
-    const token = await user.getIdToken()
-    $axios.setToken(token, 'Bearer')
-  }
+const axiosPlugin: Plugin = (context: Context) => {
+  context.$axios.interceptors.request.use(async (config: AxiosRequestConfig) => {
+    const user = await auth()
+    if (user) {
+      const token = await user.getIdToken()
+      config.headers.common.Authorization = `Bearer ${token}`
+    }
+    return config
+  })
 }
 
 export default axiosPlugin
